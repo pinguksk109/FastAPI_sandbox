df1efee362a070408d4dad88dcf7df4ec5fb0662
Keisuke Ota <k.ota109@outlook.com>
2024-07-29 02:09:44 +0900

    dynamodbのput,create,scanを実行してみた

---
+++ app/infrastructure/repository/dynamodb_repository.py
@@ -0,0 +1,86 @@
+import boto3
+from boto3.dynamodb.conditions import Attr
+from botocore.exceptions import ClientError
+from datetime import datetime, timezone
+
+class DynamodbRepository:
+    def __init__(self):
+        self.dyn_resource = boto3.resource('dynamodb', region_name='us-west-2', aws_access_key_id='local',
+        aws_secret_access_key='local', endpoint_url='http://localhost:8000')
+        self.table = self.dyn_resource.Table('ot-test')
+
+    def create(self, id, userId):
+        try:
+            current_utc_time = datetime.now(timezone.utc)
+            current_utc_time_iso = current_utc_time.isoformat()  # ISO 8601形式に変換
+            print(current_utc_time_iso)
+            self.table.put_item(
+                Item={
+                    "id": id,
+                    "userId": userId,
+                    "created_at": current_utc_time_iso
+                }
+            )
+        except ClientError as err:
+            raise
+    
+    def add(self, id, user_id):
+        try:
+            response = self.table.update_item(
+                Key={
+                    'id': id,
+                    'userId': user_id
+                },
+                UpdateExpression="set #status = :status_value",
+                ExpressionAttributeNames={
+                    '#status': 'status'
+                },
+                ExpressionAttributeValues={
+                    ':status_value': "OK"
+                },
+                ReturnValues="UPDATED_NEW"
+            )
+            print(response)
+        except ClientError as err:
+            raise
+    
+    def scan(self, userId):
+        try:
+            response = self.table.scan(
+                FilterExpression=Attr('userId').eq(userId) & Attr('created_at').between('2024-07-01', '2024-07-31')
+            )
+            return response.get('Items', [])
+
+        except ClientError as err:
+            raise
+
+        # 公式ドキュメントの書き方
+        # movies = []
+        # scan_kwargs = {
+        #     "FilterExpression": Attr('userId').eq(userId) & Attr('created_at').between('2024-07-01', '2024-07-31')
+        #     # "ProjectionExpression": "#yr, title, info.rating",
+        #     # "ExpressionAttributeNames": {"#yr": "year"},
+        # }
+        # try:
+        #     done = False
+        #     start_key = None
+        #     while not done:
+        #         if start_key:
+        #             scan_kwargs["ExclusiveStartKey"] = start_key
+        #         response = self.table.scan(**scan_kwargs)
+        #         movies.extend(response.get("Items", []))
+        #         start_key = response.get("LastEvaluatedKey", None)
+        #         done = start_key is None
+
+        # except ClientError as err:
+        #     print(f"Unexpected error: {err}")
+        #     raise
+
+        # return movies
+
+if __name__ == '__main__':
+    repository = DynamodbRepository()
+    # repository.create("3", "xxxxx")
+    # repository.add('0', 'xxxxx')
+    response = repository.scan('xxxxx')
+    print(response)
